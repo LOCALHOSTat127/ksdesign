a9ff28b6be1c6b9d36bedabdb367e2b7b6e04096
white rose <whiterose.people7050@gmail.com>
2023-03-16 11:03:01 +0530

    scss implemented in project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar/Navbar";
+
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar/>
+     
     </div>
   )
 }
@@ -14,9 +14,3 @@ const App = () => {
 export default App;
 
 
-// Make Header
-// Make Footer
-// Make Sections
-// Place into Home page
-// Add send mail function.
-// make routes & add to App.js
